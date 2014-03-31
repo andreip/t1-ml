b0d8faf24c65f71f4536457248d0a1dd8b53e2e7
Andrei Petre <p31andrei@gmail.com>
2014-03-31 22:59:09 +0300

    Send GAMES_NO to run.sh

--- run.sh
+++ run.sh
@@ -15,12 +15,18 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-if [[ $# -eq 1 ]]; then
-  PORT_SERVER_1=$1
+if [[ $# -eq 2 ]]; then
+  PORT_SERVER=$1
+  GAMES_NO=$2
+elif [[ $# -eq 1 ]]; then
+  PORT_SERVER=$1
+  GAMES_NO=200
 else
-  PORT_SERVER_1=35000
+  PORT_SERVER=35000
+  GAMES_NO=200
 fi
-echo $PORT_SERVER_1
+
+echo $GAMES_NO
 
 get_free_port()
 {
@@ -34,19 +40,15 @@ get_free_port()
 
 launch_round()
 {
-    eval "./bricks_server --port $1 --height $2 --length $3 --output-file $4 &"
+    eval "./bricks_server --port $1 --height $2 --length $3 --output-file $4 --games-no=$GAMES_NO &"
     sleep 1
     eval "./brickmaker --port $1 --in-file $5 &"
     eval "./bricklayer.py $1 &"
 }
 
 make -f Makefile.compile_d build
-launch_round $PORT_SERVER_1 4 4 score1 distributions/dist1
+launch_round $PORT_SERVER 4 4 score1 distributions/dist1
 
-#PORT_SERVER_2=35001
-##get_free_port PORT_SERVER_2
-#launch_round $PORT_SERVER_2 8 5 score2 distributions/dist2
-#
-#PORT_SERVER_3=35002
-##get_free_port PORT_SERVER_3
-#launch_round $PORT_SERVER_3 8 6 score3 distributions/dist3
+#launch_round $PORT_SERVER 8 5 score2 distributions/dist2
+
+#launch_round $PORT_SERVER 8 6 score3 distributions/dist3
